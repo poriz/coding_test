bddf537f912de02c1039e3191e8feb4baee4aad9
poriz <areacmzl@gmail.com>
2023-11-15 18:57:43 +0900

    sol

--- codes/1074.py
+++ codes/1074.py
@@ -3,7 +3,6 @@ N,r,c = map(int,sys.stdin.readline().split(" "))
 def sol(N, r, c):
     if N == 0:
         return 0
-    d = r%2
     return 2 * (r % 2) + (c % 2) + 4 * sol(N - 1, int(r / 2), int(c / 2))
 
 
