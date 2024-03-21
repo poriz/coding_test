b89ba0a740b04174a4811c1ed249c9ab5e3738ba
poriz <areacmzl@gmail.com>
2024-03-21 10:08:32 +0900

    boj) 10866 덱

---
+++ like_baaarkingDog/0x07/10866.py
@@ -0,0 +1,38 @@
+from collections import deque
+import sys
+N = int(sys.stdin.readline())
+q = deque([])
+
+for _ in range(N):
+    cmd = sys.stdin.readline().strip().split(" ")
+    if cmd[0] == 'pop_front':
+        if q:
+            print(q.popleft())
+        else:
+            print(-1)
+    elif cmd[0] == 'pop_back':
+        if q:
+            print(q.pop())
+        else:
+            print(-1)
+    elif cmd[0] == 'size':
+        print(len(q))
+    elif cmd[0] == 'empty':
+        if q:
+            print(0)
+        else:
+            print(1)
+    elif cmd[0] == 'front':
+        if q:
+            print(q[0])
+        else:
+            print(-1)
+    elif cmd[0] == 'back':
+        if q:
+            print(q[-1])
+        else:
+            print(-1)
+    elif cmd[0] == 'push_front':
+        q.appendleft(cmd[1])
+    else:
+        q.append(cmd[1])
