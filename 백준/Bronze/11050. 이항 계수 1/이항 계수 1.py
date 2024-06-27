746cb8af8d6520839c8a1cce20c407245023e60b
poriz <36216087+poriz@users.noreply.github.com>
2024-06-27 15:33:56 +0900

    [Bronze I] Title: 이항 계수 1, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -0,0 +1,18 @@
+import sys
+a,b = map(int,sys.stdin.readline().split())
+
+cache = [[0 for _ in range(b+1)] for _ in range(a+1)]
+
+
+# 하나를 뽑는것과 모두를 뽑는 가짓수는 1이다.
+for i in range(a+1):
+    cache[i][0] = 1
+
+for j in range(b+1):
+    cache[j][j] = 1
+
+# nCk = n-1Ck + n-1Ck-1
+for i in range(1,a+1):
+    for j in range(1,b+1):
+        cache[i][j] = cache[i-1][j] + cache[i-1][j-1]
+print(cache[a][b])
