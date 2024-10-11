03be355814e9ba32218274694ba8a7d99029df06
poriz <36216087+poriz@users.noreply.github.com>
2024-10-11 17:15:34 +0900

    Update 문자열 내 마음대로 정렬하기.py

--- programmers/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
+++ programmers/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
@@ -1,3 +1,22 @@
 def solution(strings, n):
+    # 내장 함수를 사용한 풀이
     answer = sorted(strings, key = lambda x:[x[n],x])
+
+    # 버블 정렬 (최초 작성)
+    # for j in range(len(strings)):
+    #     for i in range(len(strings)-1):
+    #         if strings[i][n] > strings[i+1][n]:
+    #             strings[i],strings[i+1] = strings[i+1],strings[i]
+    #         if strings[i][n] == strings[i+1][n]:
+    #             if strings[i] > strings[i+1]:
+    #                 strings[i],strings[i+1] = strings[i+1],strings[i]
+
+    # 버블 정렬 (코드 최적화)
+    # length = len(strings)
+    # for j in range(length):
+    #     for i in range(length - 1 - j):
+    #         if (strings[i][n], strings[i]) > (strings[i+1][n], strings[i+1]):
+    #             strings[i], strings[i+1] = strings[i+1], strings[i]
+
+    
     return answer
